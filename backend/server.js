2083c079e8f99f43d508d9fc41791798b2354266
Milos1988 <vojvoda1988@gmail.com>
2023-05-09 13:51:54 +0200

    set 2

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,6 @@ const port = 5050;
 
 app.listen(port, (err) => {
     err ? console.log("Error on server start")
-        : console.log(`Server is running on port ${port}...`)
+        : console.log(`Server is running on port ${port}..`)
 }
 )
