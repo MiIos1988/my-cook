aeae1f87c14bc3335b251c7ce3094a60760415b4
Milos1988 <vojvoda1988@gmail.com>
2023-05-10 12:34:21 +0200

    MongoDb is connecting

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 5050;
+const mongoose = require("mongoose")
 
+mongoose.connect("mongodb+srv://vojvoda1988:vojvoda1988@cluster0.jit59eb.mongodb.net/")
+    .then(console.log("MongoDB is connecting..."))
+    .catch(err => console.log("Error in MongoDB", err))
 
 
 
