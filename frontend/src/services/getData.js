0a37afa05d8a2ad5d9f21e9078db984bb78f25d6
Milos1988 <vojvoda1988@gmail.com>
2023-05-10 13:51:50 +0200

    Change number recipes

--- frontend/src/services/getData.js
+++ frontend/src/services/getData.js
@@ -2,7 +2,7 @@
 const getData = () => {
     const apiKey = '8f3a14a98cd545ab885a22bc44861818';
 
-    return fetch('https://api.spoonacular.com/recipes/random?apiKey=' + apiKey + '&number=100')
+    return fetch('https://api.spoonacular.com/recipes/random?apiKey=' + apiKey + '&number=10')
         .then(response => response.json())
         .then(data => {
             return data.recipes;
