d922cd430d0b71ae19f1fc1b133bfc8135346986
Milos1988 <vojvoda1988@gmail.com>
2023-06-06 23:32:20 +0200

    Create navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import ArrangeObject from "./components/ArrangeObject";
 import UseEx from "./components/UseEx";
+import NavbarComponent from "./components/NavbarComponent";
 
 const App = () => {
 
   return (
     <div className="App">
+      <NavbarComponent/>
       <UseEx/>
       <ArrangeObject />
+
       <Outlet/>
     </div>
   );
